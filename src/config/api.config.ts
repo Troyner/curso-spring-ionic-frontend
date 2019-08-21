1c3dfc1407a91f85a60230b6dc4338321790078d
Troyner <marcusvi91@gmail.com>
2019-08-21 12:52:26 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl: "https://curso-spring-ionic-troyner.s3.us-east-2.amazonaws.com"
 }
