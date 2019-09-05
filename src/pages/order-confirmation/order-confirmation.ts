723601ffa09f008946809a007e962f065fce3a2f
Troyner <marcusvi91@gmail.com>
2019-09-05 12:41:46 -0300

    Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
 
   constructor(
     public navCtrl: NavController,
@@ -55,10 +56,14 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout() {
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
-          console.log(response.headers.get('location'));
+          this.codPedido = this.extractId(response.headers.get('location'));
           this.cartService.createOrClearCart();
         },
         error => {
@@ -69,4 +74,8 @@ export class OrderConfirmationPage {
       );
   }
 
+  private extractId(location: string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
+  }
 }
