be32e537834e14e5be39e6f66c2928b0bb9c3b6d
Troyner <marcusvi91@gmail.com>
2019-09-05 12:30:21 -0300

    Salvando o pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @IonicPage()
 @Component({
@@ -23,7 +24,8 @@ export class OrderConfirmationPage {
     public navCtrl: NavController,
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService) {
     this.pedido = this.navParams.get('pedido');
   }
 
@@ -47,6 +49,24 @@ export class OrderConfirmationPage {
 
   total() : number {
     return this.cartService.total();
-  } 
+  }
+
+  back() {
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  checkout() {
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+          console.log(response.headers.get('location'));
+          this.cartService.createOrClearCart();
+        },
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage')
+          }
+        }
+      );
+  }
 
 }
